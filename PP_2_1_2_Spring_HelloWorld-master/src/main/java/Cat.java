ea05fdbfad5b62c7a8dcc0b768a4c696cd82ff6e
eduardaccount <100372621+eduardaccount@users.noreply.github.com>
2022-03-08 15:45:48 +0500

    Add files via upload

---
+++ PP_2_1_2_Spring_HelloWorld-master/src/main/java/Cat.java
@@ -0,0 +1,18 @@
+import org.springframework.context.annotation.Scope;
+
+public class Cat {
+
+    String message;
+
+    public Cat() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
